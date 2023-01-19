375a094183dd0ae0d00e15354f759cef52b8467e
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-01-20 00:15:01 +0530

    Add project files.

---
+++ MvcToDoList/Data/MvcToDoListContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlackCoderMvcToDoApp.Models;
+
+namespace BlackCoderMvcToDoApp.Data
+{
+    public class BlackCoderMvcToDoAppContext : DbContext
+    {
+        public BlackCoderMvcToDoAppContext (DbContextOptions<BlackCoderMvcToDoAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlackCoderMvcToDoApp.Models.ToDoList> ToDoList { get; set; }
+    }
+}
---
+++ MvcToDoList/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlackCoderMvcToDoApp.Data;
+
+namespace BlackCoderMvcToDoApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BlackCoderMvcToDoAppContext(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<BlackCoderMvcToDoAppContext>>()))
+            {
+                // Look for an To-Do tasks
+                if (context.ToDoList.Any())
+                {
+                    return; // DB has been seeded
+                }
+
+
+
+                context.ToDoList.AddRange(
+
+                  //  below are some dummy lists i have created for testing purpose
+
+                    new ToDoList
+                    {
+                        Task = "Do some more DSA challenges",
+                        Description = "Use resources like HackerRank or Leetcode",
+                        Status = "Planned",
+                        Priority = "High",
+                        Assgignee = "Example",
+                        Comments = "",
+                        Created = DateTime.Now.AddDays(3),
+                        CompleteBy = DateTime.Now.AddDays(5)
+                    },
+
+                    new ToDoList
+                    {
+                        Task = "Look at Linux/Ubuntu",
+                        Description = "",
+                        Status = "Planned",
+                        Priority = "High",
+                        Assgignee = "Example",
+                        Comments = "",
+                        Created = DateTime.Now.AddDays(3),
+                        CompleteBy = DateTime.Now.AddDays(5)
+                    },
+
+                    new ToDoList
+                    {
+                        Task = "Look at Linux/Ubuntu",
+                        Description = "",
+                        Status = "Planned",
+                        Priority = "High",
+                        Assgignee = "Example",
+                        Comments = "",
+                        Created = DateTime.Now.AddDays(3),
+                        CompleteBy = DateTime.Now.AddDays(5)
+                    }
+                    ); ;
+                context.SaveChanges();
+            }
+        }
+    }
+}
